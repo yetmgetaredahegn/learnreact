674fb57941a1f27e746283224a90ff3222b30bb2
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-12-03 00:04:32 +0300

    css modules

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,17 @@
-import Buttons from "./components/Buttons";
-import { useState } from "react";
-import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
+import "./App.css";
+
 function App() {
-  // let items = ["New York", "San Francisco", "Tokyo", "London", "Berlin"];
-  // const handleSelectItem = (item: string) => {
-  //   console.log(item);
-  // }
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Berlin"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
 
 
-  const [alertVisibility, setAlertVisibility] = useState(false);
+  // const [alertVisibility, setAlertVisibility] = useState(false);
   return (
     <div>
-      {alertVisibility && <Alert onClose={() => setAlertVisibility(false)}>This is an alert message!</Alert>}
-      <Buttons color="primary" onClick={() => setAlertVisibility(true)} children="btn"/>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
     </div>
   );
 }
--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./ListGroup.module.css";
 
 interface Props {
     items: string[];
@@ -13,7 +14,7 @@ function ListGroup({items,heading,onSelectItem}: Props) {
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No items found!</p>}
-      <ul className="list-group">
+      <ul className={[styles.listGroup,styles.container].join(" ")}>
         {items.map((item,index) => (
           <li
             className={
