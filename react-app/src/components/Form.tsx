70b05e6b568ba262247983b2b3e0712991500b91
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-12-03 19:35:13 +0300

    useRef

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,18 +1,25 @@
-import React, { type FormEvent } from 'react'
+import React, { useRef, type FormEvent } from 'react'
 
 const Form = () => {
+    const nameRef = useRef<HTMLInputElement>(null);
+    const ageRef = useRef<HTMLInputElement>(null);
+    const person = { name: '', age: 0 };
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
-        console.log('Form submitted');
+        if (nameRef.current)
+            person.name = nameRef.current.value
+        if (ageRef.current)
+            person.age=parseInt(ageRef.current.value)
+        console.log(person);
     }
     return (
         <form onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input type="text" className="form-control" id="name" />
+                <input ref={nameRef} type="text" className="form-control" id="name" />
             </div>
             <div className="mb-3"><label htmlFor="age" className="form-label">Age</label>
-            <input id='age' type="number" className="form-control" /></div>
+            <input ref={ageRef} id='age' type="number" className="form-control" /></div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
     )
