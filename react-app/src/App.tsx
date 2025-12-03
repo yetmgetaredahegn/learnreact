1840bfbf8dcb936a09daea3aa23a0841925827d4
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-12-03 20:36:54 +0300

    updating nested objects

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import Form from './components/Form.tsx';
 
 function App() {
+  const [customer,setCustomer] = useState({
+    name:'Yetu',
+    address: {
+      city: 'Addis Ababa',
+      zipCode: 1000
+    }
+  })
 
+  const handleClick = () => {
+    setCustomer({
+      ...customer,
+      address: { 
+        ...customer.address, 
+        zipCode: 2000 
+      }});
+  };
 
   return (
     <div>
-      <Form></Form>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
