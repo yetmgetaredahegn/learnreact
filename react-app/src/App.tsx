390b7c063e2e4c2b78b1d03795c4abe5760537dc
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-12-15 20:06:16 +0300

    handling errors

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -11,14 +11,17 @@ interface User{
 
 function App() {
   const [users,setUsers]=useState<User []>([]);
+  const [error,setError] = useState('')
 
   useEffect(()=>{
     axios.get<User []>('https://jsonplaceholder.typicode.com/users')
     .then(res=> setUsers(res.data))
+    .catch(err=> setError(err.message))
   },[])
 
   return (
     <>
+    {error && <p>{error}</p>}
   <ul>{users.map(user=> <li key={user.id}>{user.name}</li>)}</ul>
     </>
   );
