0af891dc233cad8a394d3eea29d552f2764b94a5
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-12-21 02:57:42 +0300

    add users

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -33,22 +33,36 @@ function App() {
       controller.abort();
     };
   }, [])
-  function deleteUser(user: User) {
+  const deleteUser = (user: User) => {
     const originalUsers = [...users];
     setUsers(users.filter(u => u.id !== user.id))
     axios.delete(`https://jsonplaceholder.typicode.com/users/${user.id}`)
-      .catch(err =>{ 
+      .catch(err => {
         setError(err.message);
         setUsers(originalUsers);
       });
   }
+  const addUser = () => {
+    const newUser: User = { id: 0, name: 'New User', username: '', email: '' };
+    setUsers([newUser, ...users]);
+    axios.post<User>('https://jsonplaceholder.typicode.com/users', newUser)
+      .then(res => {
+        setUsers([res.data, ...users]);
+      })
+      .catch(err => {
+        setError(err.message);
+        setUsers(users);
+      });
+  };
+
   return (
     <>
       {error && <p>{error}</p>}
+      <button className="btn btn-primary mb-3" onClick={() => addUser()}>Add users</button>
       <ul className="list-group">{users.map(user =>
         <li className="list-group-item d-flex justify-content-between" key={user.id}>
           {user.name}
-          <button className="btn btn-outline-danger" onClick={()=> deleteUser(user)}>Delete</button>
+          <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
         </li>)}
       </ul>
     </>
